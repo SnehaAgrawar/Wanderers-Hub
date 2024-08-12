4448644165219bd627b5d2759dee9a0ecf477aa9
madhuradanke <madhuramd15@gmail.com>
2024-08-13 00:22:09 +0530

    Footer designed

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,6 +2,16 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+
+    <div className="flex flex-col min-h-screen">
+      <main className="flex-1"> {/* Add any content here */}</main>
+      <footer className="items-left bg-gray-200 py-4 p-4">
+        <h6>Description:</h6>
+        <h6>Contact Us: <a href="mailto:madhuramd15@gmail.com">Mail</a></h6>
+        <h6>Github: <a href='https://github.com/SnehaAgrawar/Wanderers-Hub.git'>Wanderer's Hub</a></h6>
+        <h6>LinkedIn: <a href='https://www.linkedin.com/in/sneha-agrawar-175993267/'>Sneha</a> <br /> <a href='https://www.linkedin.com/in/madhuradanke'>Madhura</a> </h6>
+      </footer>
+    </div>
+ 
   )
 }
