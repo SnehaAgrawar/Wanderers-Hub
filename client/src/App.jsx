65b9c3e580284f012c3a08fcb0e5a6de49f5da1f
madhuradanke <madhuramd15@gmail.com>
2024-08-13 00:29:14 +0530

    merge resolved

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Header from './components/Header';
+<<<<<<< HEAD
 import Footer from './components/Footer';
+=======
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+>>>>>>> 1047610fc564d912fbed8b2caf93ffc965b26976
 
 function App() {
   return (
@@ -10,6 +16,9 @@ function App() {
     <Header></Header>
       <Routes>
         <Route path='/' element={<Home/>}></Route>
+        <Route path='/about' element={<About/>} />
+        <Route path='/sign-in' element={<SignIn/>} />
+        <Route path='/sign-up' element={<SignUp/>} />
       </Routes>
       <Footer></Footer>
     </Router>
