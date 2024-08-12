1047610fc564d912fbed8b2caf93ffc965b26976
SnehaAgrawar <snehamadhu555@gmail.com>
2024-08-13 00:20:36 +0530

    Header page designed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Header from './components/Header';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 
 function App() {
   return (
@@ -9,6 +12,9 @@ function App() {
     <Header></Header>
       <Routes>
         <Route path='/' element={<Home/>}></Route>
+        <Route path='/about' element={<About/>} />
+        <Route path='/sign-in' element={<SignIn/>} />
+        <Route path='/sign-up' element={<SignUp/>} />
       </Routes>
     </Router>
   );
